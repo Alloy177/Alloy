bbac39146bf430a9f313289f9ead7bc3cbfa000c
Alloy <98376074+Alloy177@users.noreply.github.com>
2023-10-16 09:29:50 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# 2048
+
+Just a 2048 project for my portfolio. All code, assets, etc. in the repository are all created by me.
+
+Original creator: [Gabriele Cirulli](https://github.com/gabrielecirulli/)\
+Original repository: [2048](https://github.com/gabrielecirulli/2048/)
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html"
+  ],
+  theme: {
+    extend: {
+      screens: {
+        "max-video": {
+          raw: "(aspect-ratio < 16 / 9)"
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
